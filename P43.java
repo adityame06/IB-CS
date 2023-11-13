ce6ae7104700f106df1b220194bfd5b950f3582a
Aditya Mehta <145420122+adityame06@users.noreply.github.com>
2023-11-13 18:17:30 +0800

    Update

---
+++ P43.java
@@ -0,0 +1,44 @@
+/*
+
+PrX.Y:
+--Copy and paste the corresponding problem description
+  from the worksheets here--
+  
+*/
+
+public class P43Switch
+{
+	public static void main(String[] args)
+	{
+		int n = IBIO.inputInt("Enter a number less than 1000: ");
+		boolean found = false;
+		while (n>999)
+		{
+			n = IBIO.inputInt("Enter a number less than 1000: ");
+		}
+		for(int x = 1; x <= n; x++)
+		{
+			for(int y = 1; y <= n; y++)
+				{
+					int search = x*x + y*y;
+					if(search == n)
+						{
+							found = true;
+							System.out.println(x + " " +y);
+							return;
+						}
+				}
+		}
+		if (found == false)
+			{
+				System.out.println("Impossible");
+			}
+	}
+}
+
+
+/*
+
+OUTPUT OF YOUR PROGRAM (copy & paste from Terminal/Command prompt)
+
+*/
